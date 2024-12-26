e6146b4c456932f5f0ad9c8bb27c038f70836e50
Grapeist <58837528+Grapeist@users.noreply.github.com>
2024-12-26 09:14:34 -0800

    Update vending_prices.asm
    ADDED LINKING CORDS TO VENDING MACHINES

--- data/items/vending_prices.asm
+++ data/items/vending_prices.asm
@@ -5,6 +5,7 @@ ENDM
 
 VendingPrices:
 	; item id, price
-	vend_item FRESH_WATER, 200
-	vend_item SODA_POP,    300
-	vend_item LEMONADE,    350
+	vend_item FRESH_WATER,     200
+	vend_item SODA_POP,        300
+	vend_item LEMONADE,        350
+	vend_item LINKING_CORD,    550
